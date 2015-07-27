5f4ec43c98d6956a922db0e74d13c8710d016950
dis-zephyro <zephyro@yandex.ru>
2015-07-28 00:19:29 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -14,3 +14,23 @@ $('.tasting-prev').click(function(){
 $('.tasting-next').click(function(){
     $('.tasting-slider').slick('slickNext');
 });
+
+
+$(".cert-large, .video-play").fancybox({
+    "padding" : 0
+});
+
+$(".btn-popup").fancybox({
+    "padding" : 0
+});
+
+
+
+// подсказки для форм (можноу удалить)
+
+$(function(){
+    var mr_input="input[type=text]";
+    $(mr_input).each(function() {    $(this).data('stext',$(this).val());                                               });
+    $(mr_input).focus(function(){	 $(this).val()==$(this).data('stext')  ?   $(this).val(''): "0";	               	});
+    $(mr_input).blur(function() {    $(this).val().length<=0               ?   $(this).val($(this).data('stext')): "0"; });
+});
